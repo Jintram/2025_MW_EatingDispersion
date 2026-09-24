96cfe82d18b8f64c493f95bdcd88bba87e2a5fe0
“Martijn <martijn.wehrens@gmail.com>
2026-09-24 16:51:54 +0200

    updated plots

--- regenerate_example_outputs.sh
+++ regenerate_example_outputs.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-python leafstats_example_3channels.py
-python leafstats_example_1channel.py
-python leafstats_syntheticdata.py
+conda activate 2026_leafdamage2
+
+conda run -n 2026_leafdamage2 python leafstats_example_3channels.py && echo "3channels done" &
+conda run -n 2026_leafdamage2 python leafstats_example_1channel.py && echo "1channel done" &
+conda run -n 2026_leafdamage2 python leafstats_syntheticdata.py && echo "synthetic done" &
+wait
+echo "all done"
+
